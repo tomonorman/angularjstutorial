8c49a5ebc7ed63d76b7651db1d8d0826b08e45fb
Tomo Norman <tomo.l.norman@gmail.com>
2021-03-30 20:17:46 +0900

    data seperated to json

--- app/app.js
+++ app/app.js
@@ -15,30 +15,29 @@ myTomoApp.config(['$routeProvider', function($routeProvider){
     });
 }]);
 
-myTomoApp.controller('TomoController', ['$scope',function($scope){
+myTomoApp.controller('TomoController', ['$scope', '$http', function($scope, $http){
   
-$scope.removePerson = function(person){
-  var removedPerson = $scope.people.indexOf(person);
-  $scope.people.splice(removedPerson, 1);
-};
+  $scope.removePerson = function(person){
+    var removedPerson = $scope.people.indexOf(person);
+    $scope.people.splice(removedPerson, 1);
+  };
 
-$scope.addPerson = function(){
-  $scope.people.push({
-    name: $scope.newperson.name,
-    favFood: $scope.newperson.favFood,
-    salary: parseInt($scope.newperson.salary),
-    available: true
+  $scope.addPerson = function(){
+    $scope.people.push({
+      name: $scope.newperson.name,
+      favFood: $scope.newperson.favFood,
+      salary: parseInt($scope.newperson.salary),
+      available: true
+    });
+
+    $scope.newperson.name = "";
+    $scope.newperson.favFood = "";
+    $scope.newperson.salary = "";
+  };
+
+  $http.get('data/people.json').then(function(response){
+    $scope.people = response.data;
   });
 
-  $scope.newperson.name = "";
-  $scope.newperson.favFood = "";
-  $scope.newperson.salary = "";
-};
-
-  $scope.people = [
-    { name: "Tomo", favFood: "Fish and Chips", salary: 50, available: true },
-    { name: "Patrick", favFood: "Bagel", salary: 40, available: false },
-    { name: "Sam", favFood: "Burrito", salary: 20, available: true },
-    { name: "Snickers", favFood: "Big Mac", salary: 10, available: true }
-  ];
 }]); 
+
