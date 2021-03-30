860ca80a67181fe03dc52c19f5d211f4da0b4c23
Tomo Norman <tomo.l.norman@gmail.com>
2021-03-30 16:50:55 +0900

    Input Form Added

--- app/app.js
+++ app/app.js
@@ -3,12 +3,28 @@ var myTomoApp = angular.module('myTomoApp', []);
 
 myTomoApp.controller('TomoController', ['$scope',function($scope){
   
-  $scope.message = "Hello!";
+$scope.removePerson = function(person){
+  var removedPerson = $scope.people.indexOf(person);
+  $scope.people.splice(removedPerson, 1);
+};
+
+$scope.addPerson = function(){
+  $scope.people.push({
+    name: $scope.newperson.name,
+    favFood: $scope.newperson.favFood,
+    salary: parseInt($scope.newperson.salary),
+    available: true
+  });
+
+  $scope.newperson.name = "";
+  $scope.newperson.favFood = "";
+  $scope.newperson.salary = "";
+};
 
   $scope.people = [
-    { name: "Tomo", favFood: "Fish and Chips", salary: 50 },
-    { name: "Patrick", favFood: "Bagel", salary: 40 },
-    { name: "Sam", favFood: "Burrito", salary: 20 },
-    { name: "Snickers", favFood: "Big Mac", salary: 10 }
+    { name: "Tomo", favFood: "Fish and Chips", salary: 50, available: true },
+    { name: "Patrick", favFood: "Bagel", salary: 40, available: false },
+    { name: "Sam", favFood: "Burrito", salary: 20, available: true },
+    { name: "Snickers", favFood: "Big Mac", salary: 10, available: true }
   ];
 }]); 
